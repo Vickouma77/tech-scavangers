efc5fc9dc571df5023e15233bbbbaac515fc6dfe
Vickouma77 <Vickprestone20@gmail.com>
2024-06-10 18:00:28 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import NavBar from './components/NavBar.vue';
+import Hero from './components/Hero.vue';
+import Feature from './components/Feature.vue';
+import Testimonials from './components/Testimonials.vue';
+import Team from './components/Team.vue';
+import Footer from './components/Footer.vue';
+</script>
+
+<template>
+  <NavBar />
+  <Hero />
+  <Feature />
+  <Testimonials />
+  <Team />
+  <Footer />
+</template>
+
+<style scoped>
+
+</style>
