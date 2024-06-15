b3090d0fbbc1c300b43c0b9e316b39836323fe45
Vickouma77 <Vickprestone20@gmail.com>
2024-06-15 22:10:09 +0300

    Socials links

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -14,17 +14,17 @@
                     <p>CEO/Co-founder</p>
                     <ul class="flex justify-center mt-4 space-x-4">
                         <li>
-                            <a href="#" class="text-[#39569c] hover:text-gray-900 dark:hover:text-white">
+                            <a href="https://www.facebook.com/basil.hartjacques?mibextid=ZbWKwL" class="text-[#39569c] hover:text-gray-900 dark:hover:text-white">
                                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
+                            <a href="https://x.com/akachi_sibuor?t=vVM4xWI6--7nSBuaptAASQ&s=09" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
                                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
+                            <a href="https://www.linkedin.com/in/basil-jackson-25b848214?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
                                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path fill-rule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clip-rule="evenodd"/>
                                 <path d="M7.2 8.809H4V19.5h3.2V8.809Z"/>
@@ -32,7 +32,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="text-[#f33f3f] hover:text-gray-900 dark:hover:text-white">
+                            <a href="https://www.instagram.com/jus_sibuor?igsh=dXJ3ZnFmbncxamp2" class="text-[#f33f3f] hover:text-gray-900 dark:hover:text-white">
                                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path fill="currentColor" fill-rule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clip-rule="evenodd"/>
                                 </svg>
@@ -53,12 +53,12 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
+                            <a href="https://x.com/CnarioSupreme?t=AwbLSkwN113-MOeMlzW5LA&s=09" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
                                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
+                            <a href="https://www.linkedin.com/in/seth-odiwuor?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
                                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path fill-rule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clip-rule="evenodd"/>
                                 <path d="M7.2 8.809H4V19.5h3.2V8.809Z"/>
@@ -82,17 +82,17 @@
                     <p>Head, Communication</p>
                     <ul class="flex justify-center mt-4 space-x-4">
                         <li>
-                            <a href="#" class="text-[#39569c] hover:text-gray-900 dark:hover:text-white">
+                            <a href="https://www.facebook.com/share/c4jyPPC9rcHUvj9h/?mibextid=qi2Omg" class="text-[#39569c] hover:text-gray-900 dark:hover:text-white">
                                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
+                            <a href="https://x.com/Astro_Jonn?t=SFpix2xU5rjLnRIUaM_fcw&s=09" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
                                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
+                            <a href="https://www.linkedin.com/in/johnmark-o-40053770?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
                                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path fill-rule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clip-rule="evenodd"/>
                                 <path d="M7.2 8.809H4V19.5h3.2V8.809Z"/>
@@ -100,7 +100,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="text-[#f33f3f] hover:text-gray-900 dark:hover:text-white">
+                            <a href="https://www.instagram.com/intergalacticsage?utm_source=qr&igsh=ZDM1NzFra2JmZGxs" class="text-[#f33f3f] hover:text-gray-900 dark:hover:text-white">
                                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path fill="currentColor" fill-rule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clip-rule="evenodd"/>
                                 </svg>
@@ -126,7 +126,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
+                            <a href="https://ke.linkedin.com/in/sophy-aguvasu-3a3b9b20a" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
                                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path fill-rule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clip-rule="evenodd"/>
                                 <path d="M7.2 8.809H4V19.5h3.2V8.809Z"/>
@@ -134,7 +134,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="text-[#f33f3f] hover:text-gray-900 dark:hover:text-white">
+                            <a href="https://www.instagram.com/ms.sophie.a?igsh=N280N2J2ZjBsNGRn" class="text-[#f33f3f] hover:text-gray-900 dark:hover:text-white">
                                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path fill="currentColor" fill-rule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clip-rule="evenodd"/>
                                 </svg>
